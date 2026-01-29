d213ea63b52a6c7ae465d626f19af61b33a4205b
Dewrong <ysidda33@gmail.com>
2026-01-29 16:33:39 -0600

    add history and input display

--- calculator.js
+++ calculator.js
@@ -1,52 +1,95 @@
-const add = function(first, second){
+let first = "";
+let second = "";
+let operator = "";
+let total = "";
+
+const add = function(){
     
     let total = first + second;
     return total;
 }
 
-const subtract = function(first, second){
+const subtract = function(){
 
     let total = first- second;
     return total;
 }
 
-const multiply = function(first, second){
+const multiply = function(){
     
     let total = first * second;
     return total;
 }
 
-const divide = function(first, second){
+const divide = function(){
     
     let total = first/second;
     return total;
 }
 
-const operate = function(first, operator, second){
-    
-    first = parseInt(first);
-    second = parseInt(second);
+const operate = function(inputOperator){
 
-
-    let total = 0;
-    console.log(operator);
-    switch(operator){
-        case '+':
-            total = add(first, second);
-            break;
-        case '-':
-            total = subtract(first, second);
-            break;
-        case '/':
-            total = divide(first, second);
-            break;
-        case '*':
-            total = multiply(first, second);
-            break;
-        default:
-            console.log("that didn't work");
+    //operator = inputOperator;
+    //This usecase is for when a first number is not input, to use the history's latest total as the first number    
+    if(document.getElementById("current-numbers").innerHTML == ""){        
+        //take previous total value as first
+        first = total;
+        total = 0;
+        operator = inputOperator;
+        document.getElementById("current-numbers").innerHTML = `${first} ${operator} `;
+        return;
     }
-    console.log(`${first} ${operator} ${second} = ${total}`);
-    return total;
+    if(inputOperator == "=" && operator != ""){
+        //For cases when equal is selected and operator is selected
+        //need to parse second int
+        let secondIndex = document.getElementById("current-numbers").innerHTML.indexOf(operator);
+        second = document.getElementById("current-numbers").innerHTML.slice(secondIndex+1);
+        second = parseInt(second);
+        console.log(operator);
+        switch(operator){
+            case '+':
+                total = add(first, second);
+                break;
+            case '-':
+                total = subtract(first, second);
+                break;
+            case '/':
+                total = divide(first, second);
+                break;
+            case '*':
+                total = multiply(first, second);
+                break;
+            default:
+                console.log("that didn't work");
+        }
+        document.getElementById("history").innerHTML += `\n${first}${operator}${second} = ${total}\n`;
+        console.log(`${first} ${operator} ${second} = ${total}`);
+        document.getElementById("current-numbers").innerHTML = "";
+    operator = "";
+    first = "";
+    second = "";
+    return;
+    }
+    if(operator != "" && inputOperator != "="){
+        debugger
+        document.getElementById("history").innerHTML += "Error: Can only evaluate one operation at a time."
+        clearInput();
+    }
+    else{
+        //For when operator is selected, only want to parse first number and operator right now.  
+        first = parseInt(document.getElementById("current-numbers").innerHTML);
+        operator = inputOperator;
+        document.getElementById("current-numbers").innerHTML += operator;
+    }
+}
 
+function typeToCurrent(number){
+    document.getElementById("current-numbers").innerHTML = document.getElementById("current-numbers").innerHTML + number;
+}
+
+function clearInput(){    
+    document.getElementById("current-numbers").innerHTML = "";
+    operator = "";
+    first = "";
+    second = "";
 }
